5a1825fe03b476552153deede87b1fb0eeba27cf
Ruslan <volna4753@gmail.com>
2023-03-27 23:33:31 +0300

    Урок 3
    1) Сделали экраны Рome и Authenticate
    2) Сделали Wrapper(Оболочку, где будет досутп ко всем экранам, но пока только к экрану Home)
    3) Сделали Wrapper домашним экраном

---
+++ lib/screen/wrapper.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:learning_flutter_and_firebase/screen/authenticate/authenticate.dart';
+import 'package:learning_flutter_and_firebase/screen/home/home.dart';
+
+class Wrapper extends StatelessWidget {
+  const Wrapper({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+
+    // Вернуть или экран home или authenticate
+    return Home();
+  }
+}
