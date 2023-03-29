f699b77ca472e629a035e78be34177b942895787
Ruslan <volna4753@gmail.com>
2023-03-29 21:57:18 +0300

    Урок 6
    Делаем так, чтобы после аутентификации осуществлялся переход мы переходили на домашний экран
    
    1) Добавили новый пакет провайдера, с помощью которого мы можем вызвать домашний экран после экрана аутентификации
    2) Обернули домашний в виджет StreamProvider с помощью которого мы можем сделать переход на домашний экран после аутентификации
    3) В файле wrapper.dart получаем доступ к пользовательским данным от провайдера и записываем в переменную user

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:learning_flutter_and_firebase/screen/wrapper.dart';
+import 'package:learning_flutter_and_firebase/services/auth.dart';
+import 'package:provider/provider.dart';
+import 'package:learning_flutter_and_firebase/models/user.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -14,8 +17,16 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Wrapper(),
+
+    // С помощью StreamProvider можем прослушивать потоки
+    return StreamProvider<User1?>.value(
+      // Указываем какой поток мы хотим прослушать и какие данные получить обратно
+      // в данном случае поток AuthService и оборачивает приложение MaterialApp
+      value: AuthService().user,
+      initialData: null,
+      child: MaterialApp(
+        home: Wrapper(),
+      ),
     );
   }
 }
--- lib/screen/wrapper.dart
+++ lib/screen/wrapper.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:learning_flutter_and_firebase/models/user.dart';
 import 'package:learning_flutter_and_firebase/screen/authenticate/authenticate.dart';
 import 'package:learning_flutter_and_firebase/screen/home/home.dart';
+import 'package:provider/provider.dart';
+import 'package:learning_flutter_and_firebase/models/user.dart';
 
 class Wrapper extends StatelessWidget {
   const Wrapper({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+
+    // в переменную user записываем данные типа User1 и получаем их в context
+    // крч получаем доступ к пользовательским данным от провайдера
+    final user = Provider.of<User1?>(context);
+    print(user);
 
     // Вернуть или экран home или authenticate
     return Authenticate();
