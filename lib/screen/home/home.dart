cba979d8fc65ce0aa1a2c3cb3cae8c8e6e4eef1a
Ruslan <volna4753@gmail.com>
2023-04-06 20:51:48 +0300

    Урок 21
    Делаем всплывашку для того, чтобы можно было внести изменения в плитку
    
    1) Сделали кнопку, которая вызывает функцию _showSettingsPanel(), которую мы пока не написали
    2) Сделали функцию _showSettingsPanel() на экране home, которая использует встроенный метод, который делает выпадашку

--- lib/screen/home/home.dart
+++ lib/screen/home/home.dart
@@ -14,6 +14,17 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    void _showSettingsPanel(){
+      showModalBottomSheet(context: context, builder: (context){
+        return Container(
+          padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 60.0),
+          child: Text('Bottom sheet'),
+        );
+      });
+    }
+
+
     // Теперь эта штука будет передавать, то что находится в коллекции
     return StreamProvider<List<Brew?>?>.value(
       initialData: null,
@@ -32,6 +43,11 @@ class Home extends StatelessWidget {
               onPressed: () async {
                 await _auth.signOut();
               },
+            ),
+            ElevatedButton.icon(
+                onPressed: () => _showSettingsPanel(),
+                icon: Icon(Icons.settings),
+                label: Text('Settings'),
             ),
           ],
         ),
